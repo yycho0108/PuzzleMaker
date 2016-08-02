be63f43a061f76cda29c1ca62a327d98da882370
yycho0108 <jchocholate@gmail.com>
2016-08-02 03:22:07 -0400

    unstaging everything

--- Win32Project7/Dialog.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include "resource.h"
-#include "Globals.h"
-
-BOOL CALLBACK DlgProc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
-bool UpdateDimensions(HWND hDlg);
--- Win32Project7/Globals.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-extern int brdWidth, brdHeight;
-extern int imgWidth, imgHeight;
-extern int tileWidth, tileHeight;
-extern int wndWidth, wndHeight;
-extern int twSmall, thSmall;
-extern HWND hMainWnd;
